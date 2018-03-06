c9487dab82264ea09bcb8d9eaa8c1b217e9622bb
Kirill <revenkov-k@mail.ru>
2018-03-06 09:53:58 +0300

    Test project

---
+++ Presentation/Nop.Web/Areas/Admin/Validators/Catalog/ProductValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Nop.Web.Areas.Admin.Models.Catalog;
+using Nop.Core.Domain.Catalog;
+using Nop.Data;
+using Nop.Services.Localization;
+using Nop.Web.Framework.Validators;
+
+namespace Nop.Web.Areas.Admin.Validators.Catalog
+{
+    public partial class ProductValidator : BaseNopValidator<ProductModel>
+    {
+        public ProductValidator(ILocalizationService localizationService, IDbContext dbContext)
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage(localizationService.GetResource("Admin.Catalog.Products.Fields.Name.Required"));
+            RuleFor(x => x.BookAuthor).Length(0, 100).WithMessage(localizationService.GetResource("Admin.Catalog.Products.Fields.BookAuthor.Required")); ;
+
+            SetDatabaseValidationRules<Product>(dbContext);
+        }
+    }
+}
